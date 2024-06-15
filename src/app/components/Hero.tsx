75ebb96cf009993a8c46de0e18d3c82007936e41
Vivien Phang <101629147+vivienphang@users.noreply.github.com>
2024-06-15 23:43:58 +0800

    Feat: Added all assests and some basic components

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,6 +1,10 @@
-import Image from 'next/image';
+'use client';
+import CustomButton from './CustomButton';
 
 const Hero = () => {
+  const handleScroll = () => {
+    console.log('empty');
+  };
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -10,6 +14,14 @@ const Hero = () => {
         <p className="hero__subtitle">
           Streamline your car rental with our effortless booking process.
         </p>
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+        <div className="hero__image-container">
+          <div className="hero__image"></div>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,8 @@
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   mode: 'jit',
   theme: {
